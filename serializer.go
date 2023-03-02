f7d3c18d558cf107d99ee14630c2583c29460de2
gooblitz <56411578+gooblitz@users.noreply.github.com>
2023-03-02 14:38:20 +0000

    Update serializer.go
    make serializer interface methods exported

--- serializer.go
+++ serializer.go
@@ -6,9 +6,9 @@ import (
 
 // A Serializer describes the required interface for serializers
 type Serializer interface {
-	vsn() string
-	encode(*Message) ([]byte, error)
-	decode([]byte) (*Message, error)
+	Vsn() string
+	Encode(*Message) ([]byte, error)
+	Decode([]byte) (*Message, error)
 }
 
 // JSONSerializerV1 implements the original JSON protocol, which is a JSON object with keys and values
@@ -19,11 +19,11 @@ func NewJSONSerializerV1() *JSONSerializerV1 {
 	return &JSONSerializerV1{}
 }
 
-func (s *JSONSerializerV1) vsn() string {
+func (s *JSONSerializerV1) Vsn() string {
 	return "1.0.0"
 }
 
-func (s *JSONSerializerV1) encode(msg *Message) ([]byte, error) {
+func (s *JSONSerializerV1) Encode(msg *Message) ([]byte, error) {
 	data, err := json.Marshal(msg)
 	if err != nil {
 		return nil, err
@@ -32,7 +32,7 @@ func (s *JSONSerializerV1) encode(msg *Message) ([]byte, error) {
 	return data, nil
 }
 
-func (s *JSONSerializerV1) decode(data []byte) (*Message, error) {
+func (s *JSONSerializerV1) Decode(data []byte) (*Message, error) {
 	var msg Message
 	err := json.Unmarshal(data, &msg)
 	if err != nil {
@@ -50,11 +50,11 @@ func NewJSONSerializerV2() *JSONSerializerV2 {
 	return &JSONSerializerV2{}
 }
 
-func (s *JSONSerializerV2) vsn() string {
+func (s *JSONSerializerV2) Vsn() string {
 	return "2.0.0"
 }
 
-func (s *JSONSerializerV2) encode(msg *Message) ([]byte, error) {
+func (s *JSONSerializerV2) Encode(msg *Message) ([]byte, error) {
 	jm := NewJSONMessage(*msg)
 	tmp := []any{&jm.JoinRef, &jm.Ref, &jm.Topic, &jm.Event, &jm.Payload}
 	data, err := json.Marshal(tmp)
@@ -65,7 +65,7 @@ func (s *JSONSerializerV2) encode(msg *Message) ([]byte, error) {
 	return data, nil
 }
 
-func (s *JSONSerializerV2) decode(data []byte) (*Message, error) {
+func (s *JSONSerializerV2) Decode(data []byte) (*Message, error) {
 	var jm JSONMessage
 	tmp := []any{&jm.JoinRef, &jm.Ref, &jm.Topic, &jm.Event, &jm.Payload}
 	err := json.Unmarshal(data, &tmp)
